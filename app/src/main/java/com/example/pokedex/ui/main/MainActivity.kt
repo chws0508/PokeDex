a86a6f8f306eea33ff01c61997b2ec0e791f3296
chws0508 <wooseokchoi0508@gmail.com>
2022-07-19 23:13:36 +0900

    Paging3 적용완료...

--- app/src/main/java/com/example/pokedex/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/ui/main/MainActivity.kt
@@ -2,27 +2,53 @@ package com.example.pokedex.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.viewModels
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.pokedex.R
 import com.example.pokedex.databinding.ActivityMainBinding
 import com.example.pokedex.ui.adatpter.PokemonListAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
+    private val pokemonListAdapter: PokemonListAdapter = PokemonListAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.apply {
-            lifecycleOwner=this@MainActivity
-            vm=viewModel
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.fetchPokemonList().collectLatest { response ->
+                binding.apply {
+                    pokemonListAdapter.addLoadStateListener {loadStates->
+                        progressBar.isVisible=loadStates.refresh is LoadState.Loading
+                    }
+                    vm = viewModel
+                    lifecycleOwner = this@MainActivity
+                    recyclerView.adapter = pokemonListAdapter
+                    pokemonListAdapter.submitData(response)
+                }
+            }
         }
+
+
     }
 
-
 }
+
+
+
+
